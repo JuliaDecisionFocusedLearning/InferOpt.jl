9058e8546fd06c10c99de5f9c80700fb5366a8c8
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2022-04-21 17:06:35 +0200

    Parallelize perturbed

--- src/InferOpt.jl
+++ src/InferOpt.jl
@@ -1,6 +1,8 @@
 module InferOpt
 
 using ChainRulesCore
+using Folds
+using LinearAlgebra
 using Random
 using SimpleTraits
 using SparseArrays
@@ -18,10 +20,12 @@ include("structured_svm.jl")
 export one_hot_argmax, softmax, sparsemax
 export shannon_entropy, half_square_norm
 export Interpolation
+export IsRegularizedPrediction
 export Perturbed, PerturbedCost
 export FenchelYoungLoss
 export SPOPlusLoss
 export ZeroOneLoss, GeneralStructuredLoss
+export IsStructuredLossFunction
 export StructuredSVMLoss
 
 end
