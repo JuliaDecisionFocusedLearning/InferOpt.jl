324cb58e5ffe9a8f56240023629c59c1c66fd22a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2022-07-22 15:47:02 +0200

    Rename outdated composition

--- src/utils/pushforward.jl
+++ src/utils/pushforward.jl
@@ -1,7 +1,7 @@
 """
     Pushforward{L,G}
 
-Differentiable composition of a probabilistic `layer` with an arbitrary function `post_processing`.
+Differentiable pushforward of a probabilistic `layer` with an arbitrary function `post_processing`.
 
 `Pushforward` can be used for direct regret minimization (aka learning by experience) when the post-processing returns a cost.
 
@@ -16,38 +16,38 @@ struct Pushforward{L,P}
     post_processing::P
 end
 
-function Base.show(io::IO, composition::Pushforward)
-    (; layer, post_processing) = composition
+function Base.show(io::IO, pushforward::Pushforward)
+    (; layer, post_processing) = pushforward
     return print(io, "Pushforward($layer, $post_processing)")
 end
 
 """
-    compute_probability_distribution(composition, θ)
+    compute_probability_distribution(pushforward, θ)
 
-Output the distribution of `composition.post_processing(X)`, where `X` follows the distribution defined by `composition.layer` applied to `θ`.
+Output the distribution of `pushforward.post_processing(X)`, where `X` follows the distribution defined by `pushforward.layer` applied to `θ`.
 
-This function is not differentiable if `composition.post_processing` isn't.
+This function is not differentiable if `pushforward.post_processing` isn't.
 
 See also: [`apply_on_atoms`](@ref).
 """
-function compute_probability_distribution(composition::Pushforward, θ; kwargs...)
-    (; layer, post_processing) = composition
+function compute_probability_distribution(pushforward::Pushforward, θ; kwargs...)
+    (; layer, post_processing) = pushforward
     probadist = compute_probability_distribution(layer, θ; kwargs...)
     post_processed_probadist = apply_on_atoms(post_processing, probadist; kwargs...)
     return post_processed_probadist
 end
 
 """
-    (composition::Pushforward)(θ)
+    (pushforward::Pushforward)(θ)
 
-Output the expectation of `composition.post_processing(X)`, where `X` follows the distribution defined by `composition.layer` applied to `θ`.
+Output the expectation of `pushforward.post_processing(X)`, where `X` follows the distribution defined by `pushforward.layer` applied to `θ`.
 
-Unlike [`compute_probability_distribution(composition, θ)`](@ref), this function is differentiable, even if `composition.post_processing` isn't.
+Unlike [`compute_probability_distribution(pushforward, θ)`](@ref), this function is differentiable, even if `pushforward.post_processing` isn't.
 
 See also: [`compute_expectation`](@ref).
 """
-function (composition::Pushforward)(θ::AbstractArray{<:Real}; kwargs...)
-    (; layer, post_processing) = composition
+function (pushforward::Pushforward)(θ::AbstractArray{<:Real}; kwargs...)
+    (; layer, post_processing) = pushforward
     probadist = compute_probability_distribution(layer, θ; kwargs...)
     return compute_expectation(probadist, post_processing; kwargs...)
 end
