d6784c4924d0471f2d07a45e03c82315e2c1945d
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-06-29 23:30:38 +0200

    Replace sum with mean and add propert tests

--- src/perturbed/additive.jl
+++ src/perturbed/additive.jl
@@ -69,11 +69,11 @@ function ChainRulesCore.rrule(
     Z_samples = sample_perturbations(perturbed, θ)
     probadist = compute_probability_distribution(perturbed, θ, Z_samples; kwargs...)
     function perturbed_additive_probadist_pullback(probadist_tangent)
-        weights = probadist.weights
         weights_tangent = probadist_tangent.weights
-        dθ =
-            inv(ε) *
-            sum(w * wt * Z for (w, wt, Z) in zip(weights, weights_tangent, Z_samples))
+        if length(weights_tangent) != length(Z_samples)
+            throw(ArgumentError("Probadist tangent has invalid number of atoms"))
+        end
+        dθ = inv(ε) * mean(wt * Z for (wt, Z) in zip(weights_tangent, Z_samples))
         return NoTangent(), NoTangent(), dθ
     end
     return probadist, perturbed_additive_probadist_pullback
