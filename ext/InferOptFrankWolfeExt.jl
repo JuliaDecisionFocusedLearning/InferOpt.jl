251914772118cb8bf28d8864eb0795a0961bf377
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-09-13 11:36:14 +0200

    Update DifferentiableFrankWolfe to v0.2

--- ext/InferOptFrankWolfeExt.jl
+++ ext/InferOptFrankWolfeExt.jl
@@ -1,9 +1,34 @@
 module InferOptFrankWolfeExt
 
-using DifferentiableFrankWolfe: DiffFW, LinearMaximizationOracleWithKwargs
+using DifferentiableFrankWolfe: DifferentiableFrankWolfe, DiffFW
+using DifferentiableFrankWolfe: LinearMinimizationOracle  # from FrankWolfe
+using DifferentiableFrankWolfe: IterativeLinearSolver  # from ImplicitDifferentiation
 using InferOpt: InferOpt, RegularizedFrankWolfe, FixedAtomsProbabilityDistribution
 using InferOpt: compute_expectation, compute_probability_distribution
 using LinearAlgebra: dot
+
+"""
+    LinearMaximizationOracleWithKwargs{F,K}
+Wraps a linear maximizer as a `FrankWolfe.LinearMinimizationOracle` with a sign switch and predefined keyword arguments.
+# Fields
+- `maximizer::F`: black box linear maximizer
+- `maximizer_kwargs::K`: keyword arguments passed to the maximizer whenever it is called
+"""
+struct LinearMaximizationOracleWithKwargs{F,K} <: LinearMinimizationOracle
+    maximizer::F
+    maximizer_kwargs::K
+end
+
+"""
+    FrankWolfe.compute_extreme_point(lmokw::LinearMaximizationOracleWithKwargs, direction)
+"""
+function DifferentiableFrankWolfe.compute_extreme_point(
+    lmokw::LinearMaximizationOracleWithKwargs, direction; kwargs...
+)
+    opposite_direction = -direction
+    v = lmokw.maximizer(opposite_direction; lmokw.maximizer_kwargs...)
+    return v
+end
 
 """
     compute_probability_distribution(regularized::RegularizedFrankWolfe, θ; kwargs...)
@@ -19,7 +44,8 @@ function InferOpt.compute_probability_distribution(
     f(y, θ) = Ω(y) - dot(θ, y)
     f_grad1(y, θ) = Ω_grad(y) - θ
     lmo = LinearMaximizationOracleWithKwargs(linear_maximizer, kwargs)
-    dfw = DiffFW(f, f_grad1, lmo)
+    implicit_kwargs = (; linear_solver=IterativeLinearSolver(; accept_inconsistent=true))
+    dfw = DiffFW(f, f_grad1, lmo; implicit_kwargs)
     weights, atoms = dfw.implicit(θ; frank_wolfe_kwargs=frank_wolfe_kwargs)
     probadist = FixedAtomsProbabilityDistribution(atoms, weights)
     return probadist
